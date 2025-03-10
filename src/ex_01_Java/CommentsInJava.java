7402646a821cdc7c06d607994fa68d4c8063f686
abeed <abhimohammed123@gmail.com>
2025-03-10 19:28:51 +0530

    06_03 commitng and pushing the code of java basic programs

---
+++ src/ex_01_Java/CommentsInJava.java
@@ -0,0 +1,15 @@
+package ex_01_Java;
+
+import java.sql.SQLOutput;
+
+public class CommentsInJava {
+    public static void main(String[] args) {
+      //  System.out.println();
+
+       /*System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();*/
+        System.out.println("comments in java");
+    }
+}
---
+++ src/ex_01_Java/Lab_002_Basic.java
@@ -0,0 +1,13 @@
+package ex_01_Java;
+
+public class Lab_002_Basic {
+    public static void main(String[] args) {
+        System.out.println(2+2);
+        System.out.println(30-10);
+        System.out.println(30*30);
+        System.out.println("Abidh");
+        System.out.print("Test");
+        System.out.println("Java");
+        System.out.println("Basic");
+    }
+}
