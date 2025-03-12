c5cf63fa9291ccc4cc756db1f23750389f50fe1f
abeed <abhimohammed123@gmail.com>
2025-03-12 21:27:13 +0530

    06_03 commitng and pushing Practiced exercises

---
+++ src/ex_04_Operators/Math_Functions.java
@@ -0,0 +1,13 @@
+package ex_04_Operators;
+
+public class Math_Functions {
+    public static void main(String[] args) {
+        int e=10;
+        int f=40;
+        int G=Math.max(e,f);
+        System.out.println(G);
+        int H=Math.min(f,e);
+        System.out.println(H);
+
+    }
+}
