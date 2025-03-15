8e0cd41e912e38c582b2c3c544565ae159dfd9e4
abeed <abhimohammed123@gmail.com>
2025-03-15 18:14:07 +0530

    06_03 commitng Task Completion

--- src/ex_06_ternary_operator/Ternery_largest.java
+++ src/ex_06_ternary_operator/Ternery_largest.java
@@ -5,7 +5,8 @@ public class Ternery_largest {
         int b1=400;
         int b2=500;
         int b3=600;
-        int largest=(b1>b2&&b1>b3)?b1:(b2>b3&&b2>b1)?b2:b3;
+        int b4=700;
+        int largest=(b1>b2&&b1>b3&&b1>b4)?b1:(b2>b3&&b2>b1&&b2>b4)?b2:(b3>b2&&b3>b1&&b3>b4)?b3:b4;
         System.out.println("Largest number is "+largest);
     }
 }
